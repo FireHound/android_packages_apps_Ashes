412cb79c46f224bc4a15ca86ed14e807790a35d9
Jmz <jmztaylor@gmail.com>
2016-10-06 21:37:04 +0200

    Add support to disable immersive messages [2/2]
    Change-Id: I5e7cc2e79f11570b0d24d8d9935ee6c3e199b6ca

--- src/com/fh/settings/fragments/notifications/MiscNotifications.java
+++ src/com/fh/settings/fragments/notifications/MiscNotifications.java
@@ -16,29 +16,61 @@
 
 package com.fh.settings.fragments.notifications;
 
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.SystemProperties;
+import android.os.UserHandle;
+import android.provider.Settings;
 import android.support.v7.preference.ListPreference;
-import android.support.v14.preference.SwitchPreference;
 import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceScreen;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.support.v14.preference.SwitchPreference;
 
 import com.android.internal.logging.nano.MetricsProto;
-
-import android.content.res.Resources;
-import com.fh.settings.R;
+import com.android.settings.development.DevelopmentSettings;
 import com.android.settings.SettingsPreferenceFragment;
-import com.android.settings.Utils;
 
-public class MiscNotifications extends SettingsPreferenceFragment {
+import com.fh.settings.R;
+
+public class MiscNotifications extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+
     private static final String TAG = "NotificationsSettings";
+    private static final String DISABLE_IMMERSIVE_MESSAGE = "disable_immersive_message";
+    private SwitchPreference mDisableIM;
 
-    @Override
-    public int getMetricsCategory() {
-        return MetricsProto.MetricsEvent.FH_SETTINGS;
-
-    }
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.misc_notifications);
+
+    ContentResolver resolver = getActivity().getContentResolver();
+
+    mDisableIM = (SwitchPreference) findPreference(DISABLE_IMMERSIVE_MESSAGE);
+        mDisableIM.setOnPreferenceChangeListener(this);
+        int DisableIM = Settings.System.getInt(getContentResolver(),
+                DISABLE_IMMERSIVE_MESSAGE, 0);
+        mDisableIM.setChecked(DisableIM != 0);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
+        if (preference == mDisableIM) {
+            boolean value = (Boolean) newValue;
+            Settings.System.putInt(getContentResolver(), DISABLE_IMMERSIVE_MESSAGE,
+                    value ? 1 : 0);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.FH_SETTINGS;
     }
 }
