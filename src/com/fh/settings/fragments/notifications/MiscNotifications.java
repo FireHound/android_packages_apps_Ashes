a42e81372941ea4fd3a21eefb613022a8063ded8
ezio84 <brabus84@gmail.com>
2018-01-25 18:28:00 +0300

    Allow to suppress notifications sound/vibration if screen is on [2/2]
    Change-Id: Id168e967d7b69bfd7feb609f04d3ab3d2087108c

--- src/com/fh/settings/fragments/notifications/MiscNotifications.java
+++ src/com/fh/settings/fragments/notifications/MiscNotifications.java
@@ -16,29 +16,66 @@
 
 package com.fh.settings.fragments.notifications;
 
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.SystemProperties;
+import android.os.UserHandle;
+import android.provider.Settings;
 import android.support.v7.preference.ListPreference;
-import android.support.v14.preference.SwitchPreference;
 import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceScreen;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.support.v14.preference.SwitchPreference;
 
 import com.android.internal.logging.nano.MetricsProto;
-
-import android.content.res.Resources;
-import com.fh.settings.R;
+import com.android.settings.development.DevelopmentSettings;
 import com.android.settings.SettingsPreferenceFragment;
-import com.android.settings.Utils;
 
-public class MiscNotifications extends SettingsPreferenceFragment {
+import com.fh.settings.R;
+
+public class MiscNotifications extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
     private static final String TAG = "NotificationsSettings";
+
+    private ListPreference mNoisyNotification;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.misc_notifications);
+
+        ContentResolver resolver = getActivity().getContentResolver();
+
+        mNoisyNotification = (ListPreference) findPreference("notification_sound_vib_screen_on");
+        int mode = Settings.System.getIntForUser(resolver,
+                Settings.System.NOTIFICATION_SOUND_VIB_SCREEN_ON,
+                1, UserHandle.USER_CURRENT);
+        mNoisyNotification.setValue(String.valueOf(mode));
+        mNoisyNotification.setSummary(mNoisyNotification.getEntry());
+        mNoisyNotification.setOnPreferenceChangeListener(this);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
+        if (preference == mNoisyNotification) {
+            int value = Integer.parseInt((String) newValue);
+            Settings.System.putIntForUser(resolver,
+                    Settings.System.NOTIFICATION_SOUND_VIB_SCREEN_ON, value, UserHandle.USER_CURRENT);
+            int index = mNoisyNotification.findIndexOfValue((String) newValue);
+            mNoisyNotification.setSummary(
+                    mNoisyNotification.getEntries()[index]);
+            return true;
+        }
+        return false;
+    }
 
     @Override
     public int getMetricsCategory() {
         return MetricsProto.MetricsEvent.FH_SETTINGS;
 
-    }
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        addPreferencesFromResource(R.xml.misc_notifications);
     }
 }
