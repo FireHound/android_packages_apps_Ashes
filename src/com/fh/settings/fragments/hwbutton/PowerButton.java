6a5aff9ac42e8834992151f767b13f72e0e11a55
PMS22 <prathams99@rediff.com>
2018-01-26 13:23:39 +0530

    Ashes: Update power menu customizations
    - Kill old power menu style
    - Use Lineage Settings for advanced reboot
    
    Change-Id: Ia51555a2575fecb81a4dc3c0875207af5ca0781b
    Signed-off-by: PMS22 <prathams99@rediff.com>

--- src/com/fh/settings/fragments/hwbutton/PowerButton.java
+++ src/com/fh/settings/fragments/hwbutton/PowerButton.java
@@ -23,6 +23,7 @@ import android.content.pm.UserInfo;
 import android.os.Bundle;
 import android.os.UserHandle;
 import android.os.UserManager;
+import android.support.v7.preference.CheckBoxPreference;
 import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceCategory;
@@ -42,6 +43,12 @@ import java.util.List;
 
 import com.fh.settings.utils.Utils;
 
+import lineageos.providers.LineageSettings;
+
+import org.lineageos.internal.util.PowerMenuConstants;
+
+import static org.lineageos.internal.util.PowerMenuConstants.*;
+
 public class PowerButton extends SettingsPreferenceFragment
                 implements Preference.OnPreferenceChangeListener {
 
@@ -49,6 +56,13 @@ public class PowerButton extends SettingsPreferenceFragment
     private static final String KEY_POWERMENU_TORCH = "powermenu_torch";
 
     private SwitchPreference mPowermenuTorch;
+    private CheckBoxPreference mScreenshotPref;
+    private CheckBoxPreference mAirplanePref;
+    private CheckBoxPreference mUsersPref;
+
+    Context mContext;
+    private ArrayList<String> mLocalUserConfig = new ArrayList<String>();
+    private String[] mAllActions;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -67,10 +81,70 @@ public class PowerButton extends SettingsPreferenceFragment
         mPowermenuTorch.setChecked((Settings.System.getInt(resolver,
                 Settings.System.POWERMENU_TORCH, 0) == 1));
         }
+        mAllActions = PowerMenuConstants.getAllActions();
+
+        for (String action : mAllActions) {
+            if (action.equals(GLOBAL_ACTION_KEY_SCREENSHOT)) {
+                mScreenshotPref = (CheckBoxPreference) findPreference(GLOBAL_ACTION_KEY_SCREENSHOT);
+            } else if (action.equals(GLOBAL_ACTION_KEY_AIRPLANE)) {
+                mAirplanePref = (CheckBoxPreference) findPreference(GLOBAL_ACTION_KEY_AIRPLANE);
+            } else if (action.equals(GLOBAL_ACTION_KEY_USERS)) {
+                mUsersPref = (CheckBoxPreference) findPreference(GLOBAL_ACTION_KEY_USERS);
+//            } else if (action.equals(GLOBAL_ACTION_KEY_BUGREPORT)) {
+//                mBugReportPref = (CheckBoxPreference) findPreference(GLOBAL_ACTION_KEY_BUGREPORT);
+            }
+        }
+
+        getUserConfig();
     }
 
+     @Override
+    public void onStart() {
+        super.onStart();
+
+        if (mScreenshotPref != null) {
+            mScreenshotPref.setChecked(settingsArrayContains(GLOBAL_ACTION_KEY_SCREENSHOT));
+        }
+
+        if (mAirplanePref != null) {
+            mAirplanePref.setChecked(settingsArrayContains(GLOBAL_ACTION_KEY_AIRPLANE));
+        }
+
+        if (mUsersPref != null) {
+            if (!UserHandle.MU_ENABLED || !UserManager.supportsMultipleUsers()) {
+                getPreferenceScreen().removePreference(findPreference(GLOBAL_ACTION_KEY_USERS));
+                mUsersPref = null;
+            } else {
+                List<UserInfo> users = ((UserManager) mContext.getSystemService(
+                        Context.USER_SERVICE)).getUsers();
+                boolean enabled = (users.size() > 1);
+                mUsersPref.setChecked(settingsArrayContains(GLOBAL_ACTION_KEY_USERS) && enabled);
+                mUsersPref.setEnabled(enabled);
+            }
+        }
+
     @Override
-    public boolean onPreferenceChange(Preference preference, Object newValue) {
+    public boolean onPreferenceTreeClick(Preference preference) {
+        boolean value;
+
+        if (preference == mScreenshotPref) {
+            value = mScreenshotPref.isChecked();
+            updateUserConfig(value, GLOBAL_ACTION_KEY_SCREENSHOT);
+
+        } else if (preference == mAirplanePref) {
+            value = mAirplanePref.isChecked();
+            updateUserConfig(value, GLOBAL_ACTION_KEY_AIRPLANE);
+
+        } else if (preference == mUsersPref) {
+            value = mUsersPref.isChecked();
+            updateUserConfig(value, GLOBAL_ACTION_KEY_USERS);
+
+/*
+        } else if (preference == mBugReportPref) {
+            value = mBugReportPref.isChecked();
+            updateUserConfig(value, GLOBAL_ACTION_KEY_BUGREPORT);
+*/
+
         if (preference == mPowermenuTorch) {
             boolean value = (Boolean) newValue;
             Settings.System.putInt(getActivity().getContentResolver(),
@@ -78,6 +152,72 @@ public class PowerButton extends SettingsPreferenceFragment
             return true;
         }
         return false;
+        } else {
+            return super.onPreferenceTreeClick(preference);
+        }
+        return true;
+    }
+
+    private void updateUserConfig(boolean enabled, String action) {
+        if (enabled) {
+            if (!settingsArrayContains(action)) {
+                mLocalUserConfig.add(action);
+            }
+        } else {
+            if (settingsArrayContains(action)) {
+                mLocalUserConfig.remove(action);
+            }
+        }
+        saveUserConfig();
+    }
+
+    private void getUserConfig() {
+        mLocalUserConfig.clear();
+        String[] defaultActions;
+        String savedActions = LineageSettings.Secure.getStringForUser(mContext.getContentResolver(),
+                LineageSettings.Secure.POWER_MENU_ACTIONS, UserHandle.USER_CURRENT);
+
+        if (savedActions == null) {
+            defaultActions = mContext.getResources().getStringArray(
+                    com.android.internal.R.array.config_globalActionsList);
+            for (String action : defaultActions) {
+                mLocalUserConfig.add(action);
+            }
+        } else {
+            for (String action : savedActions.split("\\|")) {
+                mLocalUserConfig.add(action);
+            }
+        }
+    }
+
+    private void saveUserConfig() {
+        StringBuilder s = new StringBuilder();
+
+        ArrayList<String> setactions = new ArrayList<String>();
+        for (String action : mAllActions) {
+            if (settingsArrayContains(action)) {
+                setactions.add(action);
+            } else {
+                continue;
+            }
+        }
+
+        for (int i = 0; i < setactions.size(); i++) {
+            s.append(setactions.get(i).toString());
+            if (i != setactions.size() - 1) {
+                s.append("|");
+            }
+        }
+
+        LineageSettings.Secure.putStringForUser(getContentResolver(),
+                LineageSettings.Secure.POWER_MENU_ACTIONS, s.toString(), UserHandle.USER_CURRENT);
+        updatePowerMenuDialog();
+    }
+
+    private void updatePowerMenuDialog() {
+        Intent u = new Intent();
+        u.setAction(lineageos.content.Intent.ACTION_UPDATE_POWER_MENU);
+        mContext.sendBroadcastAsUser(u, UserHandle.ALL);
     }
 
     @Override
