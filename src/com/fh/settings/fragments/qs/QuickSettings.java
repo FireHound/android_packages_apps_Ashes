e3d0513492c4e18131181b5040de4bc602437e7e
Juan Ezquerro LLanes <arrase@gmail.com>
2018-08-28 08:48:25 +0200

    Ashes: QS panel opacity [2/2]
    Change-Id: Iad2942bfc17d1802a061f334bfa54796137ba0d9

--- src/com/fh/settings/fragments/qs/QuickSettings.java
+++ src/com/fh/settings/fragments/qs/QuickSettings.java
@@ -37,11 +37,17 @@ import java.util.Locale;
 import android.text.TextUtils;
 import android.view.View;
 
+import com.fh.settings.preferences.CustomSeekBarPreference;
+
 import java.util.List;
 import java.util.ArrayList;
 
 public class QuickSettings extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener {
+
+    private static final String KEY_QS_PANEL_ALPHA = "qs_panel_alpha";
+
+    private CustomSeekBarPreference mQsPanelAlpha;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -49,10 +55,24 @@ public class QuickSettings extends SettingsPreferenceFragment implements
         addPreferencesFromResource(R.xml.quick_settings);
         PreferenceScreen prefScreen = getPreferenceScreen();
         ContentResolver resolver = getActivity().getContentResolver();
+
+        mQsPanelAlpha = (CustomSeekBarPreference) findPreference(KEY_QS_PANEL_ALPHA);
+        int qsPanelAlpha = Settings.System.getInt(getContentResolver(),
+                Settings.System.QS_PANEL_BG_ALPHA, 221);
+        mQsPanelAlpha.setValue((int)(((double) qsPanelAlpha / 255) * 100));
+        mQsPanelAlpha.setOnPreferenceChangeListener(this);
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
+        if (preference == mQsPanelAlpha) {
+            int bgAlpha = (Integer) newValue;
+            int trueValue = (int) (((double) bgAlpha / 100) * 255);
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.QS_PANEL_BG_ALPHA, trueValue);
+            return true;
+        }
         return false;
     }
 
