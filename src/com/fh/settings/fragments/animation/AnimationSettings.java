3028379b49ffc1dba1c389f1d2ce3a2e1bfb3048
PMS22 <prathams99@rediff.com>
2019-11-03 15:40:47 +0000

    Ashes: Arrange fragments properly
    Signed-off-by: PMS22 <prathams99@rediff.com>

--- src/com/fh/settings/fragments/animation/AnimationSettings.java
+++ src/com/fh/settings/fragments/animation/AnimationSettings.java
@@ -43,16 +43,75 @@ import java.util.ArrayList;
 public class AnimationSettings extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener {
 
+    private static final String PREF_TILE_ANIM_STYLE = "qs_tile_animation_style";
+    private static final String PREF_TILE_ANIM_DURATION = "qs_tile_animation_duration";
+    private static final String PREF_TILE_ANIM_INTERPOLATOR = "qs_tile_animation_interpolator";
+
+    private ListPreference mTileAnimationStyle;
+    private ListPreference mTileAnimationDuration;
+    private ListPreference mTileAnimationInterpolator;
+
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
         addPreferencesFromResource(R.xml.animation_settings);
         PreferenceScreen prefScreen = getPreferenceScreen();
         ContentResolver resolver = getActivity().getContentResolver();
+
+        mTileAnimationStyle = (ListPreference) findPreference(PREF_TILE_ANIM_STYLE);
+        int tileAnimationStyle = Settings.System.getIntForUser(resolver,
+                Settings.System.ANIM_TILE_STYLE, 0,
+                UserHandle.USER_CURRENT);
+        mTileAnimationStyle.setValue(String.valueOf(tileAnimationStyle));
+        mTileAnimationStyle.setSummary(mTileAnimationStyle.getEntry());
+        mTileAnimationStyle.setOnPreferenceChangeListener(this);
+
+        mTileAnimationDuration = (ListPreference) findPreference(PREF_TILE_ANIM_DURATION);
+        int tileAnimationDuration = Settings.System.getIntForUser(resolver,
+                Settings.System.ANIM_TILE_DURATION, 2000,
+                UserHandle.USER_CURRENT);
+        mTileAnimationDuration.setValue(String.valueOf(tileAnimationDuration));
+        mTileAnimationDuration.setSummary(mTileAnimationDuration.getEntry());
+        mTileAnimationDuration.setEnabled(tileAnimationStyle > 0);
+        mTileAnimationDuration.setOnPreferenceChangeListener(this);
+
+        mTileAnimationInterpolator = (ListPreference) findPreference(PREF_TILE_ANIM_INTERPOLATOR);
+        int tileAnimationInterpolator = Settings.System.getIntForUser(resolver,
+                Settings.System.ANIM_TILE_INTERPOLATOR, 0,
+                UserHandle.USER_CURRENT);
+        mTileAnimationInterpolator.setValue(String.valueOf(tileAnimationInterpolator));
+        mTileAnimationInterpolator.setSummary(mTileAnimationInterpolator.getEntry());
+        mTileAnimationInterpolator.setEnabled(tileAnimationStyle > 0);
+        mTileAnimationInterpolator.setOnPreferenceChangeListener(this);
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
+        if (preference == mTileAnimationStyle) {
+            int value = Integer.valueOf((String) newValue);
+            int index = mTileAnimationStyle.findIndexOfValue((String) newValue);
+            Settings.System.putIntForUser(resolver, Settings.System.ANIM_TILE_STYLE,
+                    value, UserHandle.USER_CURRENT);
+            mTileAnimationStyle.setSummary(mTileAnimationStyle.getEntries()[index]);
+            mTileAnimationDuration.setEnabled(value > 0);
+            mTileAnimationInterpolator.setEnabled(value > 0);
+            return true;
+       } else if (preference == mTileAnimationDuration) {
+            int value = Integer.valueOf((String) newValue);
+            int index = mTileAnimationDuration.findIndexOfValue((String) newValue);
+            Settings.System.putIntForUser(resolver, Settings.System.ANIM_TILE_DURATION,
+                    value, UserHandle.USER_CURRENT);
+            mTileAnimationDuration.setSummary(mTileAnimationDuration.getEntries()[index]);
+            return true;
+       } else if (preference == mTileAnimationInterpolator) {
+            int value = Integer.valueOf((String) newValue);
+            int index = mTileAnimationInterpolator.findIndexOfValue((String) newValue);
+            Settings.System.putIntForUser(resolver, Settings.System.ANIM_TILE_INTERPOLATOR,
+                    value, UserHandle.USER_CURRENT);
+            mTileAnimationInterpolator.setSummary(mTileAnimationInterpolator.getEntries()[index]);
+            return true;
+        }
         return false;
     }
 
